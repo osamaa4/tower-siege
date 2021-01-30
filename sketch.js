0e0ca3632c9d6f0d1855c9f3043d22b6564a24e1
osamaa4 <74911988+osamaa4@users.noreply.github.com>
2021-01-30 17:20:57 +0000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,19 @@
+block1 = new Block(30,235,30,40);
+block2 = new Block(60,235,30,40);
+block3 = new Block(90,235,30,40);
+block4 = new Block(120,235,30,40);
+block5 = new Block(360,235,30,40);
+block6 = new Block(360,235,30,40);
+block7 = new Block(360,235,30,40);
+//level two
+block8 = new Block(330,235,30,40);
+block9 = new Block(360,235,30,40);
+block10 = new Block(390,235,30,40);
+block11 = new Block(420,235,30,40);
+block12 = new Block(450,235,30,40);
+//level three
+block13 = new Block(480,235,30,40);
+block14 = new Block(510,235,30,40);
+block15 = new Block(540,235,30,40);
+//top
+block16 = new Block(570,235,30,40);
