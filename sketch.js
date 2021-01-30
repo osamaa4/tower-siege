effa03e6bd6c8b831c5a17b269b1add3fb7512b2
osamaa4 <74911988+osamaa4@users.noreply.github.com>
2021-01-30 17:29:09 +0000

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,19 +1,34 @@
-block1 = new Block(30,235,30,40);
-block2 = new Block(60,235,30,40);
-block3 = new Block(90,235,30,40);
-block4 = new Block(120,235,30,40);
-block5 = new Block(360,235,30,40);
-block6 = new Block(360,235,30,40);
-block7 = new Block(360,235,30,40);
-//level two
-block8 = new Block(330,235,30,40);
-block9 = new Block(360,235,30,40);
-block10 = new Block(390,235,30,40);
-block11 = new Block(420,235,30,40);
-block12 = new Block(450,235,30,40);
-//level three
-block13 = new Block(480,235,30,40);
-block14 = new Block(510,235,30,40);
-block15 = new Block(540,235,30,40);
-//top
-block16 = new Block(570,235,30,40);
+var car,wall;
+var speed, weight;
+speed=random(55,90)
+weight=random(400,1500)
+wall=createSprite(1500,200, 60,height/2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function setup() {
+  createCanvas(800,400);
+  createSprite(400, 200, 50, 50);
+}
+
+function draw() {
+  background(255,255,255);  
+  drawSprites();
+
+}
